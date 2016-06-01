65c8d2c31d101137f3c5a978b629ff517622e868
Alex Fisher <afisher117@googlemail.com>
2016-06-01 21:12:45 +0100

    initial routing

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/:domain', function(request, response) {
+  response.render('pages/domain', {domain: request.params.domain});
+});
+
+app.get('/:domain/courses', function(request, response) {
+  response.render('pages/courses', {domain: request.params.domain});
+});
+
+app.get('/:domain/material', function(request, response) {
+  response.render('pages/material', {domain: request.params.domain});
+});
+
+app.get('/:domain/sessions', function(request, response) {
+  response.render('pages/sessions', {domain: request.params.domain});
+});
+
+app.get('/:domain/users', function(request, response) {
+  response.render('pages/users', {domain: request.params.domain});
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
